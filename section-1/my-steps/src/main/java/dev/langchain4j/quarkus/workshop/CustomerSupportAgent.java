1998f84a31b8d236f0bbe8985f1e6919d43b14e5
Jochen Riedlinger <j_ri@gmx.de>
2026-09-04 09:20:56 +0200

    feat: step-07 - Function Calling and Tools

--- section-1/my-steps/src/main/java/dev/langchain4j/quarkus/workshop/CustomerSupportAgent.java
+++ section-1/my-steps/src/main/java/dev/langchain4j/quarkus/workshop/CustomerSupportAgent.java
@@ -2,6 +2,7 @@ package dev.langchain4j.quarkus.workshop;
 
 import dev.langchain4j.service.SystemMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
+import io.quarkiverse.langchain4j.ToolBox;
 import io.smallrye.mutiny.Multi;
 import jakarta.enterprise.context.SessionScoped;
 
@@ -13,6 +14,12 @@ public interface CustomerSupportAgent {
             You are a customer support agent of a car rental company 'Miles of Smiles'.
             You are friendly, polite and concise.
             If the question is unrelated to car rental, you should politely redirect the customer to the right department.
+
+            When calling tools or functions, strictly use JSON objects,
+            do not wrap in quotes or use plain strings.
+
+            Today is {current_date}.
             """)
+    @ToolBox(BookingRepository.class)
     Multi<String> chat(String userMessage);
 }
